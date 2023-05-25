e32522b23d71380bf9995dbbc8d9c616574009e2
Benfi4rthur <arthur_benfica@hotmail.com>
2023-05-25 17:51:51 -0300

    /dashboard

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -4,36 +4,36 @@ import { Container, Header, LogImg, Title, MenuContainer, MenuItemLink } from '.
 import { MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp } from 'react-icons/md';
 
 const Aside: React.FC = () => {
-    return (
-        <div>
-            <Container>
-                <Header>
-                    <LogImg src={logoImg} alt='Logo aplicação' />
-                    <Title>Minha Carteira</Title>
-                </Header>
-                <MenuContainer>
-                    <MenuItemLink href='/dashboard'>
-                        {' '}
-                        <MdDashboard />
-                        Dashboard
-                    </MenuItemLink>
-                    <MenuItemLink href='/lists/entradas'>
-                        {' '}
-                        <MdArrowDownward />
-                        Entradas
-                    </MenuItemLink>
-                    <MenuItemLink href='/lists/saidas'>
-                        <MdArrowUpward />
-                        Saídas
-                    </MenuItemLink>
-                    <MenuItemLink href='#'>
-                        <MdExitToApp />
-                        Sair
-                    </MenuItemLink>
-                </MenuContainer>
-            </Container>
-        </div>
-    );
+  return (
+    <div>
+      <Container>
+        <Header>
+          <LogImg src={logoImg} alt='Logo aplicação' />
+          <Title>Minha Carteira</Title>
+        </Header>
+        <MenuContainer>
+          <MenuItemLink href='/dashboard'>
+            {' '}
+            <MdDashboard />
+            Dashboard
+          </MenuItemLink>
+          <MenuItemLink href='/dashboard/lists/entradas'>
+            {' '}
+            <MdArrowDownward />
+            Entradas
+          </MenuItemLink>
+          <MenuItemLink href='/dashboard/lists/saidas'>
+            <MdArrowUpward />
+            Saídas
+          </MenuItemLink>
+          <MenuItemLink href='#'>
+            <MdExitToApp />
+            Sair
+          </MenuItemLink>
+        </MenuContainer>
+      </Container>
+    </div>
+  );
 };
 
 export default Aside;
